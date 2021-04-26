6f56261d5f8d189290bb67e8f998aa4eda715eab
Htaipham1907 <39405425+Htaipham1907@users.noreply.github.com>
2021-04-27 03:57:48 +0700

    add-more-test

--- fe-testing/src/contact/Contact/Contact.test.jsx
+++ fe-testing/src/contact/Contact/Contact.test.jsx
@@ -4,44 +4,44 @@ import userEvent from '@testing-library/user-event';
 import Contact from './Contact';
 
 
-// //snapshot
-// test('match snapshot', () => {
-//   const tree = renderer.create(<Contact/>).toJSON();
-//   expect(tree).toMatchSnapshot();
-// });
+//snapshot
+test('match snapshot', () => {
+  const tree = renderer.create(<Contact/>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
 
-// //simulate
-// test('display err text when user did not type in input field', async () => {
-//     const { getByPlaceholderText, getByText } = render(<Contact />);
-//     const input = getByPlaceholderText('Enter your issue')
-//     fireEvent.blur(input);
-//     let error;
-//     await waitFor(() => {
-//       error = getByText('Required field');
-//     });
-//     expect(error).toBeTruthy();
-//   });
-
-
-// test('display err text when user did not type in message field', async () => {
-//     const { getByPlaceholderText, getByText } = render(<Contact />);
-//     const message = getByPlaceholderText('Enter your message')
-//     fireEvent.blur(message);
-//     let error;
-//     await waitFor(() => {
-//       error = getByText('Required field');
-//     });
-//     expect(error).toBeTruthy();
-//   });
+//simulate
+test('display err text when user did not type in input field', async () => {
+    const { getByPlaceholderText, getByText } = render(<Contact />);
+    const input = getByPlaceholderText('Enter your issue')
+    fireEvent.blur(input);
+    let error;
+    await waitFor(() => {
+      error = getByText('Required field');
+    });
+    expect(error).toBeTruthy();
+  });
 
 
-// // expected output 
-// test('input field recieve correct value from user', async () => {
-//     const { getByPlaceholderText } = render(<Contact />);
-//     const input = getByPlaceholderText('Enter your issue')
-//     await userEvent.type(input, 'Tai Pham', { delay: 1 });
-//     expect(input.value).toBe('Tai Pham');
-//   });
+test('display err text when user did not type in message field', async () => {
+    const { getByPlaceholderText, getByText } = render(<Contact />);
+    const message = getByPlaceholderText('Enter your message')
+    fireEvent.blur(message);
+    let error;
+    await waitFor(() => {
+      error = getByText('Required field');
+    });
+    expect(error).toBeTruthy();
+  });
+
+
+// expected output 
+test('input field recieve correct value from user', async () => {
+    const { getByPlaceholderText } = render(<Contact />);
+    const input = getByPlaceholderText('Enter your issue')
+    await userEvent.type(input, 'Tai Pham', { delay: 1 });
+    expect(input.value).toBe('Tai Pham');
+  });
 
 
 it("Form can be submited", () => {
